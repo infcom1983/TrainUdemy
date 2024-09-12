9dc740ffb25f52cd0fa39f54aa5416fca50d6d7d
infcom <infcom@yandex.ru>
2024-09-13 00:55:00 +0300

    new

---
+++ src/aacdemy/devonline/java/basic/section06_array/ArrayErrors.java
@@ -0,0 +1,9 @@
+package aacdemy.devonline.java.basic.section06_array;
+
+public class ArrayErrors {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println(array[-1]);
+    }
+}

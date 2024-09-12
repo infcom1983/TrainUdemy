49100efc77145486de937b47e605a8ef628b3210
infcom <infcom@yandex.ru>
2024-09-12 16:38:26 +0300

    new

--- src/aacdemy/devonline/java/basic/section05_cycles/Proverks.java
+++ src/aacdemy/devonline/java/basic/section05_cycles/Proverks.java
@@ -3,5 +3,7 @@ package aacdemy.devonline.java.basic.section05_cycles;
 public class Proverks {
     public static void main(String[] args) {
         var a = 10;
+        for (int i = a; i < 10; i++) ;
+        System.out.println(a);
     }
 }

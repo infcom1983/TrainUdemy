e32dc761849c5ce680cdd07fa99ec7ad881c08cd
infcom1983 <infcom@yandex.ru>
2024-09-13 04:51:00 +0300

    new

---
+++ src/aacdemy/devonline/java/basic/section07_String/StringIntro.java
@@ -0,0 +1,13 @@
+package aacdemy.devonline.java.basic.section07_String;
+
+import java.util.Arrays;
+
+public class StringIntro {
+    public static void main(String[] args) {
+        var s1 = "hello";
+        System.out.println(s1);
+
+        char[] array = {'h', 'e', 'l', 'l', 'o'};
+        System.out.println(Arrays.toString(array));
+    }
+}

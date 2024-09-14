26bb7650a354c56ab0c17fcf49ba6b8f80eb5b6c
infcom1983 <infcom@yandex.ru>
2024-09-14 16:26:34 +0300

    new

---
+++ src/aacdemy/devonline/java/basic/section08_functions/FunctionOverloadingEx.java
@@ -0,0 +1,9 @@
+package aacdemy.devonline.java.basic.section08_functions;
+
+public class FunctionOverloadingEx {
+    public static void main(String[] args) {
+        String s = "hello";
+
+
+    }
+}
---
+++ src/aacdemy/devonline/java/basic/section08_functions/SumLibrary.java
@@ -0,0 +1,8 @@
+package aacdemy.devonline.java.basic.section08_functions;
+
+public class SumLibrary {
+    public static void main(String[] args) {
+
+
+    }
+}

4d12cfe04688d5c382cde7a227e2ba012c6da1f2
infcom1983 <infcom@yandex.ru>
2024-09-26 21:38:19 +0300

    new

---
+++ src/aacdemy/devonline/java/basic/section11/ExitFromProgram.java
@@ -0,0 +1,10 @@
+package aacdemy.devonline.java.basic.section11;
+
+public class ExitFromProgram {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.exit(2);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
---
+++ src/aacdemy/devonline/java/basic/section11/VerifierForGitCommitMessages.java
@@ -0,0 +1,7 @@
+package aacdemy.devonline.java.basic.section11;
+
+public class VerifierForGitCommitMessages {
+    public static void main(String[] args) {
+
+    }
+}

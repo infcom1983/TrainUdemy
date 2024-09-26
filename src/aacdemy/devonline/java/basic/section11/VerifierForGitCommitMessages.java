2b14532a0c2a531558512c3944e5897342df87fc
infcom1983 <infcom@yandex.ru>
2024-09-26 23:39:01 +0300

    new

--- src/aacdemy/devonline/java/basic/section11/VerifierForGitCommitMessages.java
+++ src/aacdemy/devonline/java/basic/section11/VerifierForGitCommitMessages.java
@@ -1,6 +1,13 @@
 package aacdemy.devonline.java.basic.section11;
 
+import java.util.Arrays;
+
 public class VerifierForGitCommitMessages {
+
+    private static final String[] VERBS = {
+            "Summarize",
+    };
+
     public static void main(String[] args) {
         String commitMessage =
                 "Summarize changes in around 50 characters or less\n" +
@@ -19,7 +26,97 @@ public class VerifierForGitCommitMessages {
         validateGitCommitMessage(commitMessage);
     }
 
-    private static void validateGitCommitMessage(final String commitMessage){
+    private static void validateGitCommitMessage(final String commitMessage) {
+        String[] lines = splitMessageByLineSeparator(commitMessage);
+
+        String subject = lines[0];
+        verifyThatSubjectNotEmpty(subject);
+        verifyThatSubjectSeparatedFromBody(lines);
+        verifyThatSubjectLengthLessThan50Chars(subject);
+        verifyThatSubjectIsCapitalized(subject);
+        verifyThatSubjectDoesNotEndWithPeriod(subject);
+        verifyThatSubjectIsStartedFromVerbInImperativeMood(subject);
+        if (isBodyFound(lines)) {
+            verifyThatEachBodyLineLessThan72Chars(subArray(lines, 2));
+        }
         System.out.println("Message is valid!");
+    }
+
+    private static String[] splitMessageByLineSeparator(String commitMessage) {
+        return commitMessage.split("\n");
+    }
+
+    private static void verifyThatSubjectNotEmpty(String subject) {
+        if (subject.isEmpty()) {
+            System.out.println("Enter a commit message!");
+            System.exit(10);
+        }
+    }
+
+    private static void verifyThatSubjectSeparatedFromBody(String[] lines) {
+        if (lines.length > 1 && !lines[1].isEmpty()) {
+            System.out.println("Separate subject from body with a blank line!");
+            System.exit(1);
+        }
+    }
+
+    private static void verifyThatSubjectLengthLessThan50Chars(String subject) {
+        if (subject.length() > 50) {
+            System.out.println("Limit the subject line to 50 characters! Current length is " + subject.length());
+            System.exit(2);
+        }
+    }
+
+    private static void verifyThatSubjectIsCapitalized(String subject) {
+        if (!Character.isUpperCase(subject.charAt(0))) {
+            System.out.println("Capitalize the subject line!");
+            System.exit(3);
+        }
+    }
+
+    private static void verifyThatSubjectDoesNotEndWithPeriod(String subject) {
+        char lastChar = subject.charAt(subject.length() - 1);
+        if (lastChar == '.') {
+            System.out.println("Do not end the subject line with a `" + lastChar + "` character!");
+        }
+    }
+
+    private static void verifyThatSubjectIsStartedFromVerbInImperativeMood(String subject) {
+        String firstWord = getFirstWord(subject);
+        if (!isVerb(firstWord)) {
+            System.out.println("The `" + firstWord + "` is not a verb or is not a verb in imperative mood!");
+            System.exit(5);
+        }
+    }
+
+    private static String getFirstWord(String subject) {
+        return subject.split(" ")[0];
+    }
+
+    private static boolean isVerb(String word) {
+        for (final String verb : VERBS) {
+            if (verb.equals(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isBodyFound(String[] lines) {
+        return lines.length > 2;
+    }
+
+    private static void verifyThatEachBodyLineLessThan72Chars(String[] lines) {
+        for (final String line : lines) {
+            if (line.length() > 72) {
+                System.out.println("Wrap the body at 72 characters! The following line has '" + line + "' characters: " + line.length());
+                System.exit(6);
+            }
+        }
+    }
+
+    private static String[] subArray(String[] lines, int startIndex) {
+        return Arrays.copyOfRange(lines, startIndex, lines.length);
+
     }
 }
